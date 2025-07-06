2929b5a9c096e513e4edbd0a3785aa1cbf0c1bac
ryLAn3404 <05duos_scribes@icloud.com>
2025-07-07 00:54:15 +0100

    modify a file

--- 4pKP/GpNoG/vt7/Gmo/teaching-methods_0MhW_1748121487.txt.sh
+++ 4pKP/GpNoG/vt7/Gmo/teaching-methods_0MhW_1748121487.txt.sh
@@ -1,5 +1,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
 #brew install ghi
-echo "# Formal-Methods-Courses" > README.md
 echo "Here we collect worldwide courses teaching formal methods" >> README.md
 rm tmp.txt
