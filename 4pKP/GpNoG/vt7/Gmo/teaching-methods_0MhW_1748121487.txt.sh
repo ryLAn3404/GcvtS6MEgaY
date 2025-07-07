e1c4afac932ec46114035c808da9ceaf13374146
ryLAn3404 <05duos_scribes@icloud.com>
2025-07-07 08:23:28 +0100

    Adjust the file's caching configuration for better performance

--- 4pKP/GpNoG/vt7/Gmo/teaching-methods_0MhW_1748121487.txt.sh
+++ 4pKP/GpNoG/vt7/Gmo/teaching-methods_0MhW_1748121487.txt.sh
@@ -1,4 +1,3 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
 #brew install ghi
 echo "Here we collect worldwide courses teaching formal methods" >> README.md
-rm tmp.txt
